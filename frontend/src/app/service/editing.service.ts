ab3396fccc9357180600d87c4cfde6f611d56c58
elvis992035@gmail.com <elvis992035@gmail.com>
2018-11-16 00:57:48 -0500

    editing

--- frontend/src/app/service/editing.service.ts
+++ frontend/src/app/service/editing.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditingService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postCustomer(customer): Promise {
+    return this.http.post('/api/customers', customer)
+                    .toPromise()
+                    .then((data) => data);
+  }
+
+  
+
+
 }
