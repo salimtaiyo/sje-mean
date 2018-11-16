044abd5ffb6570cdff695a00b348545276dd26f2
Elvis <elvis992035@gmail.com>
2018-11-16 16:17:07 -0500

    template page - add field and remove field are done

--- frontend/src/app/components/template/template.component.ts
+++ frontend/src/app/components/template/template.component.ts
@@ -5,9 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css']
 })
+
 export class TemplateComponent implements OnInit {
 
+  private fieldArray: Array<any> = []; // to store the field that the user added
+  private newAttribute: any = {}; // the value of the field
+
   constructor() { }
+
+  addField() {
+
+    this.fieldArray.push(this.newAttribute);
+
+}
+
+  removeField(index) {
+
+    this.fieldArray.splice (index, 1);
+
+  }
 
   ngOnInit() {
   }
